a503fbeb2c6680200e19db65192059fef51f7a1b
DESKTOP-1CL2VLB\matte <matte_920319@live.se>
2022-01-12 11:01:43 +0100

    Created a centrilise component For index page of diffrent entities

--- src/pages/genres/IndexGenres.tsx
+++ src/pages/genres/IndexGenres.tsx
@@ -1,67 +1,19 @@
-import axios, { AxiosResponse } from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import { genreDTO } from '../../models/genres/genres.model'
-import Button from '../../utils/Button'
-import customConfirm from '../../utils/customConfirm'
-import DisplayErrors from '../../utils/DisplayErrors'
-import DisplayGenericList from '../../utils/DisplayGenericList'
 import { urlGenrePagnation, urlGenres } from '../../utils/endpoints'
-import Pagination from '../../utils/Pagination'
-import SelectRecordsPerPage from '../../utils/SelectRecordsPerPage'
+import IndexEntity from '../../utils/IndexEntity'
+
 export default function IndexGenres() {
-    const [genres, setGenres] = useState<genreDTO[]>([]);
-    const [error, setError] = useState<string[]>();
-    const [totalAmountOfPages, setTOtalAmountOfPages] = useState(0);
-    const [recordsPerPage, setRecordsPerPage] = useState(5);
-    const [page, setPage] = useState(1);
-    // set pages and recordsPerPage as dependency so it reruns when they change.
-    useEffect(() => {
-        loadData();
-        console.log(totalAmountOfPages)
-        console.log(recordsPerPage)
-
-        console.log(page)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [page, recordsPerPage])
-
-    function loadData() {
-        axios.get(urlGenrePagnation, {
-            params: { page, recordsPerPage }
-        })
-            .then((response: AxiosResponse<genreDTO[]>) => {
-                const totalAmountOfRecords = parseInt(response.headers["totalamountofrecords"], 10);
-                console.log(totalAmountOfRecords)
-                setTOtalAmountOfPages(Math.ceil(totalAmountOfRecords / recordsPerPage));
-                setGenres(response.data)
-            })
-    }
-
-    async function deletegenre(id: number) {
-        try {
-            await axios.delete(`${urlGenres}/${id}`);
-            loadData();
-        } catch (error) {
-            if (error && error.response) {
-                setError(error.response.data);
-            }
-        }
-
-    }
 
     return (
         <>
-            <h3>Genres</h3>
-            <DisplayErrors errors={error} />
-            <Link className='my-2 btn btn-primary' to="/genres/create">Create Genre</Link>
-
-            <SelectRecordsPerPage onChange={(amountOfRecords) => {
-                setPage(1);
-                setRecordsPerPage(amountOfRecords)
-            }} />
-
-            <Pagination currentPage={page} totalAmountOfPages={totalAmountOfPages} onChange={newPage => setPage(newPage)} />         <DisplayGenericList list={genres}>
-                <table className='table table-striped'>
+            <IndexEntity<genreDTO>
+                entityName='Genres'
+                baseURL={urlGenres}
+                pagnationURL={urlGenrePagnation}
+                createURL='genres/create'
+                editURL='genres/edit'
+            >
+                {(genres, buttons) => <>
                     <thead>
                         <tr>
                             <th></th>
@@ -71,8 +23,7 @@ export default function IndexGenres() {
                     <tbody>
                         {genres?.map(genre => <tr key={genre.id}>
                             <td>
-                                <Link className='btn btn-success' to={`/genres/edit/${genre.id}`}>Edit</Link>
-                                <Button className='btn btn-danger ms-1' onClick={() => customConfirm(() => deletegenre(genre.id))}>Delete</Button>
+                                {buttons(genre.id)}
                             </td>
                             <td>
                                 {genre.name}
@@ -80,8 +31,9 @@ export default function IndexGenres() {
                         </tr>)}
                     </tbody>
 
-                </table>
-            </DisplayGenericList>
+                </>}
+
+            </IndexEntity>
         </>
     )
 }
---
+++ src/utils/IndexEntity.tsx
@@ -0,0 +1,85 @@
+import axios, { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from 'react'
+import { ReactElement } from 'react-markdown/lib/react-markdown';
+import { Link } from 'react-router-dom';
+import Button from './Button';
+import customConfirm from './customConfirm';
+import DisplayErrors from './DisplayErrors';
+import DisplayGenericList from './DisplayGenericList';
+import Pagination from './Pagination';
+import SelectRecordsPerPage from './SelectRecordsPerPage';
+
+export default function IndexEntity<T>(props: IndexProps<T>) {
+
+    const [entities, setEntity] = useState<T[]>([]);
+    const [error, setError] = useState<string[]>();
+    const [totalAmountOfPages, setTOtalAmountOfPages] = useState(0);
+    const [recordsPerPage, setRecordsPerPage] = useState(5);
+    const [page, setPage] = useState(1);
+    // set pages and recordsPerPage as dependency so it reruns when they change.
+    useEffect(() => {
+        loadData();
+        console.log(totalAmountOfPages)
+        console.log(recordsPerPage)
+
+        console.log(page)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [page, recordsPerPage])
+
+    function loadData() {
+        axios.get(props.pagnationURL, {
+            params: { page, recordsPerPage }
+        })
+            .then((response: AxiosResponse<T[]>) => {
+                const totalAmountOfRecords = parseInt(response.headers["totalamountofrecords"], 10);
+                console.log(totalAmountOfRecords)
+                setTOtalAmountOfPages(Math.ceil(totalAmountOfRecords / recordsPerPage));
+                setEntity(response.data)
+            })
+    }
+
+    async function deleteEntity(id: number) {
+        try {
+            await axios.delete(`${props.baseURL}/${id}`);
+            loadData();
+        } catch (error) {
+            if (error && error.response) {
+                setError(error.response.data);
+            }
+        }
+
+    }
+    const buttons = (id: number) => <>
+        <Link className='btn btn-success' to={`${props.editURL}/${id}`}>Edit</Link>
+        <Button className='btn btn-danger ms-1' onClick={() => customConfirm(() => deleteEntity(id))}>Delete</Button>
+    </>
+    return (
+        <>
+            <h3>{props.entityName}</h3>
+            <DisplayErrors errors={error} />
+            <Link className='my-2 btn btn-primary' to={props.createURL}>Create {props.entityName}</Link>
+
+            <SelectRecordsPerPage onChange={(amountOfRecords) => {
+                setPage(1);
+                setRecordsPerPage(amountOfRecords)
+            }} />
+
+            <Pagination currentPage={page} totalAmountOfPages={totalAmountOfPages} onChange={newPage => setPage(newPage)} />
+
+            <DisplayGenericList list={entities}>
+                <table className='table table-striped'>
+                    {props.children(entities!, buttons)}
+
+                </table>
+            </DisplayGenericList>
+        </>
+    )
+}
+interface IndexProps<T> {
+    entityName: string;
+    pagnationURL: string;
+    baseURL: string;
+    createURL: string;
+    editURL: string;
+    children(entites: T[], buttons: (id: number) => ReactElement): ReactElement;
+}
